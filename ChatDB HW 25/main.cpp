bcc6a9ba0853b7760383fbbae3a906cc37884831
nordica200925 <nordica200925@gmail.com>
2023-10-20 10:08:53 +0500

    Добавьте файлы проекта.

---
+++ ChatDB HW 25/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <Windows.h>
+#include <sql.h>
+#include <sqlext.h>
+#include <iostream>
+#include <string>
+#include "database.h"
+#include "chat.h"
+
+void chatMenu();
+
+int main() {
+    chatMenu();
+    return 0;
+}
